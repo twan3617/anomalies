a19d4ae2cdc71d8e77e7665b8fe1e6b8c10fde89
yeechin18 <93772920+yeechin18@users.noreply.github.com>
2022-01-30 18:41:23 +1100

    CAC Investigation
    - investigating CAC 'false positive' dips in healthy data, from just stumpy.stump as well as from FLOSS
    - comparison tests:
    --- concatenated the same healthy test 10 times and replaced the first 10 healthy tests with this repeated test. finding a moving dip that follows the fifth identical test. see fake-healthytest1repeated-welchonly-10fps.gif
    --- replaced all healthy data with random numbers. CAC is behaving as expected. see rdmdata-replace-healthy-welchonly-10fps.gif

--- bookshelf/code/streaming/scripts.py
+++ bookshelf/code/streaming/scripts.py
@@ -1,18 +1,18 @@
 ### file for scripts
 ### idea: have separate functions for cleaning data from Building_Model.mat, for performing welch then vmd, and for concatenating data together into features_seq.
 ### this gives us more flexibility if we want to modify things halfway through the process.
-### data processing function 
+### data processing function
 
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
 from scipy.signal import welch
 import stumpy
 from stumpy.floss import _cac
 from vmdpy import VMD
-import ot 
+import ot
 import matplotlib.pyplot as plt
-from matplotlib import animation 
-from IPython.display import HTML 
+from matplotlib import animation
+from IPython.display import HTML
 
 
 def exp_list_process(data_dict, cases, num_healthy):
@@ -35,28 +35,28 @@ def exp_list_process(data_dict, cases, num_healthy):
 
             df_query = df[query1 & query2 & query3]
             df_list_temp.append(df_query.reset_index(drop=True)) # Organize cases per sensor
-            
+
         df_list[sensor_str] = pd.concat(df_list_temp) # Concatenate cases across each sensor
-        
-        # Removing 50-num_healthy D00 (heahlthy i.e. non damage) cases 
+
+        # Removing 50-num_healthy D00 (heahlthy i.e. non damage) cases
         # Because a lot of the damage case only have 5 events recorded in total
         # We shall have 50-num_healthy D00 --> 5 DB0 --> 5 DBB --> 5 DBB cases in a sequence
         df_list[sensor_str] =  df_list[sensor_str].iloc[50-num_healthy:,:].reset_index(drop=True)
-    
-    return df_list 
+
+    return df_list
 
 def welch_vmd(df_list, signal_length=8192, nperseg=1024, fs=1600, vmd=0, alpha=0, tau=0, DC=0, init=0, tol=0):
     '''
-    Input: 
+    Input:
     data: df_list, a dict with Sensorn as key and all of the data (including labels).
     vmd: a natural number (if vmd > 0, perform vmd with num_modes = vmd; if 0, do not perform vmd)
-    
-    In terms of the data pipeline, this should be placed after the correct experimental data has been pulled out of Building_Model.mat into df_list. Just iterate over the number of sensors. 
-    This gives us flexibility in case we want to experiment with different combinations of normal/anomalous data etc. 
-    '''  
 
-    
-    if vmd >= 1: 
+    In terms of the data pipeline, this should be placed after the correct experimental data has been pulled out of Building_Model.mat into df_list. Just iterate over the number of sensors.
+    This gives us flexibility in case we want to experiment with different combinations of normal/anomalous data etc.
+    '''
+
+
+    if vmd >= 1:
         first_item = next(iter(df_list.values()))
         num_experiments = len(first_item)
 
@@ -84,12 +84,12 @@ def welch_vmd(df_list, signal_length=8192, nperseg=1024, fs=1600, vmd=0, alpha=0
                     # Paper for welch: http://bobweigel.net/csi763/images/Welch_1967.pdf
                     pxx = welch(u[j,:], fs=fs, nperseg=nperseg) # Returns len 2 tuple
                     vmd_list.append(np.log(pxx[1]/max(pxx[1]))) # Feature Engineering
-                
+
                 building_features[sensor_str][experiment_str] = np.array(vmd_list)
         return building_features
-    
-    if vmd == 0: 
-        # if df_list contains other things 
+
+    if vmd == 0:
+        # if df_list contains other things
         df_list = df_list.iloc[:,:8192]
 
         building_features = dict()
@@ -102,10 +102,10 @@ def welch_vmd(df_list, signal_length=8192, nperseg=1024, fs=1600, vmd=0, alpha=0
             experiment_str = f'Experiment{i+1}'
             data_row = df_list.iloc[i,:8192]
             data_row_demeaned = data_row - np.mean(data_row) # Remove "DC" component (i.e. de-mean)
-            
+
             pxx = welch(data_row_demeaned, fs=1600, nperseg=nperseg) # Returns len 2 tuple
             welch_list.append(np.log(pxx[1]/max(pxx[1]))) # Feature Engineering
-            
+
             building_features[experiment_str] = np.array(welch_list)
 
             welch_list.clear()
@@ -121,23 +121,23 @@ def data_sequencing(building_features, vmd):
         building_features_seq = dict()
         for i in range(1,num_sensors+1):
             sensor_str = f'Sensor{i}'
-            
+
             stack_list = []
             for j in range(1,num_experiments+1):
                 experiment_str = f'Experiment{j}'
                 stack_list.append(building_features[sensor_str][experiment_str])
-                
-            building_features_seq[sensor_str] = np.hstack(stack_list)
-        return building_features_seq 
 
-    if vmd == 0: 
+            building_features_seq[sensor_str] = np.hstack(stack_list)
+        return building_features_seq
+
+    if vmd == 0:
         num_experiments = len(building_features.keys())
         return np.hstack([building_features[f'Experiment{i+1}'] for i in range(num_experiments)]).squeeze()
-        
+
 
 def animate_regime_change(building_seq, num_modes, stream, regime_change_idxs, start_list_size):
     '''
-    Input: building_seq data 
+    Input: building_seq data
     start_list_size was originally 513*5
     '''
     windows = []
@@ -151,8 +151,8 @@ def animate_regime_change(building_seq, num_modes, stream, regime_change_idxs, s
 
     new_data = building_seq.T[513*5:]
 
-    for i, t in enumerate(new_data): 
-        
+    for i, t in enumerate(new_data):
+
         #update the window of x values we are currently looking at CAC for
         current_x_window = current_x_window[1:]
         current_x_window.append(i+start_list_size)
@@ -171,19 +171,19 @@ def animate_regime_change(building_seq, num_modes, stream, regime_change_idxs, s
             for change in regime_change_idxs:
                 if change in current_x_window:
                     regime_changes_window_idxs.append(current_x_window.index(change))
-                    
+
             #layer 1 pooling over modes
             sub_sample_rate = 10 #for fast OT processing
             A = cac_list.T
             B = A[::sub_sample_rate,:] # Sub-sampling to increase OT processing
-            M = ot.utils.dist0(B.shape[0]) # Ground Metric 
-            M /= M.max()  # Normalizing ground metric 
+            M = ot.utils.dist0(B.shape[0]) # Ground Metric
+            M /= M.max()  # Normalizing ground metric
             M*=1e+4 # Tuning ground metric for problem (hyper-param)
 
-            bary_wass = ot.barycenter_unbalanced(B, M, reg=5e-4, reg_m=1e-1) # reg - Entropic Regularization 
+            bary_wass = ot.barycenter_unbalanced(B, M, reg=5e-4, reg_m=1e-1) # reg - Entropic Regularization
 
             current_cac_1d = np.repeat(bary_wass,10)
-            
+
             windows.append((stream['vmd1'].T_, current_cac_1d, regime_changes_window_idxs))
 
 
@@ -229,25 +229,135 @@ def animate_regime_change(building_seq, num_modes, stream, regime_change_idxs, s
 
     HTML(anim_out)
 
+def animate_regime_change_from_scratch(building_seq, regime_change_idxs, start_list_size, m, L):
+    '''
+    Input: a time series in np.array form, with time along the first dimension
+            no capacity to deal with VMD
+            regime change indices - a list, with the index locations in the time series of the regime changes
+            m, L - hyperparameters for MP.
+    Outputs: returns anim FuncAnimation object - animated CAC moving curve with regime change red vertical lines. no need to initiate the FLOSS object
+            so that anim object can be saved or displayed.
+            to save:
+            writergif = animation.PillowWriter(fps = 10)
+            anim.save('fake-healthytest1repeated-welchonly-10fps.gif',writer=writergif)
+            to display:
+            anim_out = anim.to_jshtml()
+            plt.close()  # Prevents duplicate image from displaying
+            if os.path.exists("None0000000.png"):
+                os.remove("None0000000.png")  # Delete rogue temp file
+            HTML(anim_out)
+    '''
+
+    old_data = building_seq[:start_list_size]
+
+    mp = stumpy.stump(old_data, m=m)
+    cac_1d = _cac(mp[:, 3], L, bidirectional=False, excl_factor=1)  # This is for demo purposes only. Use floss() below!
+    stream = stumpy.floss(mp, old_data, m=m, L=L, excl_factor=1)
+
+
+    windows = []
+
+    # regime_change_idxs = []
+    # for n_exp in [10,15,20]:
+    #     regime_change_idxs.append(n_exp*513)
+
+    current_x_window = list(np.arange(start_list_size))
+
+
+    new_data = building_seq[start_list_size:]
+
+    windows = []
+
+    # regime_change_idxs = []
+    # for n_exp in [10,15,20]:
+    #     regime_change_idxs.append(n_exp*513)
+
+    for i, t in enumerate(new_data):
+        stream.update(t)
+
+        #update the window of x values we are currently looking at CAC for
+        current_x_window = current_x_window[1:]
+        current_x_window.append(i+513*5)
+
+        if i % 100 == 0:
+            #note any indices of regime changes in this x values window
+            regime_changes_window_idxs = [0]
+            for change in regime_change_idxs:
+                if change in current_x_window:
+                    regime_changes_window_idxs.append(current_x_window.index(change))
+            windows.append((stream.T_, stream.cac_1d_, regime_changes_window_idxs))
+
+    #     if (i+1) % 513*5b == 0:
+
+
+    fig, axs = plt.subplots(2, sharex=True, gridspec_kw={'hspace': 0})
+
+    axs[0].set_xlim((0, mp.shape[0]))
+    Y_MIN = min(np.min(old_data), np.min(new_data))
+    Y_MAX = max(np.max(old_data), np.max(new_data))
+    axs[0].set_ylim(Y_MIN, Y_MAX)
+    axs[1].set_xlim((0, mp.shape[0]))
+    axs[1].set_ylim((-0.1, 1.1))
+
+    #legend here^ try
+
+    lines = []
+
+    for ax in axs: #create empty structures for each frame
+        line, = ax.plot([], [], lw=2)
+        lines.append(line)
+        line, = ax.plot([],[], linewidth=2, color= 'red')
+        lines.append(line)
+    line, = axs[1].plot([], [], lw=2) # create structure for orange curve
+    lines.append(line)
+
+    def init():
+        for line in lines:
+            line.set_data([], [])
+        return lines
+
+    def animate(window):
+        data_out, cac_out, regime_changes = window
+    #     for line, data in zip(lines, [data_out, regime_changes, cac_out, regime_changes, cac_1d]):
+    #         line.set_data(np.arange(data.shape[0]), data)
+        lines[0].set_data(np.arange(data_out.shape[0]), data_out)
+        lines[2].set_data(np.arange(cac_out.shape[0]), cac_out)
+    #     lines[4].set_data(np.arange(cac_1d.shape[0]), cac_1d)
+        rgm_change = max(regime_changes)
+        lines[1].set_data([rgm_change,rgm_change],[Y_MIN, Y_MAX])
+        lines[3].set_data([rgm_change,rgm_change],[-0.1, 1.1])
+        return lines
+
+    anim = animation.FuncAnimation(fig, animate, init_func=init,
+                                   frames=windows, interval=100,
+                                   blit=True)
+    return anim
+
+
+
+    # anim_out = anim.to_jshtml()
+    # plt.close()  # Prevents duplicate image from displaying
+    #
+    # HTML(anim_out)
 
 def zero_out(data, length_zero, length_data, num_experiments, zero=True, rand=True):
     '''
-    Given a pandas dataframe data (e.g. data_dict['Sensor1']), 
+    Given a pandas dataframe data (e.g. data_dict['Sensor1']),
     return a dataframe with random segments either zeroed out (zero==true) or
     replaced by a random normal segment horizontally (along the columns).
     if random==True, provide a random integer length to be zeroed out.
     num_experiments: number of experiments to zero out in each dataframe
     '''
-    if rand == True: 
+    if rand == True:
         a, b = length_zero
         return_len = np.random.randint(a,b)
-    else: 
-        return_len = length_zero 
+    else:
+        return_len = length_zero
 
     for i in range(num_experiments):
         rand_pos = np.random.randint(0, length_data - return_len)
         if zero == True:
             data.iloc[i,rand_pos:rand_pos+return_len] = 0
-        else: 
+        else:
             data.iloc[i,rand_pos:rand_pos+return_len] = np.random.normal(loc=0, scale=1, size=return_len)
     return data
